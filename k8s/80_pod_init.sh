a6bd2e5619c2fa6528f3c5023eeffeb7e1674691
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-01-13 10:43:13 -0800

    Add xenon and fermigrid (Nova)

--- k8s/80_pod_init.sh
+++ k8s/80_pod_init.sh
@@ -28,5 +28,7 @@ groupadd -g 3001 osg && useradd -u 3001 -g 3001 -s /bin/bash osg
 groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
 groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
 groupadd -g 3004 ligo && useradd -u 3004 -g 3004 -s /bin/bash ligo
+groupadd -g 3005 xenon.biggrid.nl && useradd -u 3005 -g 3005 -s /bin/bash xenon.biggrid.nl
+groupadd -g 3006 fermigli && useradd -u 3006 -g 3006 -s /bin/bash fermigli
 
 echo ${OSG_COLLECTOR_PRP_SERVICE_HOST} /etc/condor/cond
