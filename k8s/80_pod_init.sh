5fe29db5ef0affd51be1247f589c61921910a999
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-01-06 10:06:50 -0800

    Update

--- k8s/80_pod_init.sh
+++ k8s/80_pod_init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+#
+# Make symlinks
+#
+
 # Point to the collector
 # The value is porpagated in the environment by the osg_collector_prp service 
 echo "CONDOR_HOST = ${OSG_COLLECTOR_PRP_SERVICE_HOST}" > /etc/condor/config.d/50_condor_host.config
@@ -7,17 +12,40 @@ echo "CONDOR_HOST = ${OSG_COLLECTOR_PRP_SERVICE_HOST}" > /etc/condor/config.d/50
 # If this is the first use of the persistent disk areas, create the necessary dirs
 # This dirs are normally created by the RPM in the base image
 
+if [ -n "/mnt/ceph/condor" ]; then
+  mkdir /mnt/ceph/condor && chown condor:condor /mnt/ceph/condor
+fi
+mv -f /var/lib/condor /var/lib/condor.org
+ln -s /mnt/ceph/condor /var/lib/condor && chown -h condor:condor /var/lib/condor
+
 if [ -n "/var/lib/condor/spool" ]; then
   mkdir /var/lib/condor/spool   && chown condor:condor /var/lib/condor/spool
   mkdir /var/lib/condor/execute && chown condor:condor /var/lib/condor/execute
 fi
-chown condor:condor /var/lib/condor
+
+
+###
+
+if [ -n "/mnt/ceph/condor-ce" ]; then
+  mkdir /mnt/ceph/condor-ce && chown condor:condor /mnt/ceph/condor-ce
+fi
+mv -f /var/lib/condor-ce /var/lib/condor-ce.org
+ln -s /mnt/ceph/condor-ce /var/lib/condor-ce && chown -h condor:condor /var/lib/condor-ce
+
 
 if [ -n "/var/lib/condor-ce/spool" ]; then
   mkdir /var/lib/condor-ce/spool   && chown condor:condor /var/lib/condor-ce/spool
   mkdir /var/lib/condor-ce/execute && chown condor:condor /var/lib/condor-ce/execute
 fi
-chown condor:condor /var/lib/condor-ce
+
+###
+
+if [ -n "/mnt/ceph/home" ]; then
+  mkdir /mnt/ceph/home
+fi
+mv -f /home /home.org
+ln -s /mnt/ceph/home /home
+
 
 if [ -n "/home/condor" ]; then
   mkdir /home/condor && chown condor:condor /home/condor
