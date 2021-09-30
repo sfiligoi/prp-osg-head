be51b0cb8bb394c6da07076d6d0ea97f4ebf846b
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-09-30 10:44:30 -0700

    Update 80_pod_init.sh, add creation of namespace_map

--- k8s/80_pod_init.sh
+++ k8s/80_pod_init.sh
@@ -29,4 +29,17 @@ groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
 groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
 groupadd -g 3004 ligo && useradd -u 3004 -g 3004 -s /bin/bash ligo
 
+
+cat > /etc/condor/namespace_map << EOF
+# user namespace
+* icecube osg-icecube
+* glow osg-icecube
+* ligo osg-ligo
+* osg osg-opportunistic
+* xenon.biggrid.nl osg-opportunistic
+* fermigli osg-opportunistic
+EOF
+
+
+
 echo ${OSG_COLLECTOR_PRP_SERVICE_HOST} /etc/condor/cond
